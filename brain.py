8204b975f90318d1244d96c025ea0a84b5819b20
imran6363 <156570238+imran6363@users.noreply.github.com>
2026-04-30 13:34:24 +0600

    Update brain.py

--- brain.py
+++ brain.py
@@ -2,7 +2,7 @@ from openai import OpenAI
 from config import OPENAI_API_KEY
 from memory import get_context, save_memory
 
-client = OpenAI(api_key=OPENAI_API_KEY)
+client = OpenAI(api_key=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)
 
 def ask_ai(prompt):
     context = get_context()
